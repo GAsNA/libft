453213388ab6e6ee32153ca48f8c09606bc6e1ec
Romane LESEUR <rleseur@e2r9p17.clusters.42paris.fr>
2021-11-25 11:50:53 +0100

    correction memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,9 @@
 /*   By: rleseur <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 12:41:39 by rleseur           #+#    #+#             */
-/*   Updated: 2021/11/24 12:04:12 by rleseur          ###   ########.fr       */
+/*   Updated: 2021/11/25 11:50:31 by rleseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -20,19 +18,17 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	if (!dst || !src)
 		return (0);
-	if (len == 0)
-		return (dst);
 	d = (char *)dst;
 	s = (char *)src;
 	if (d > s)
 	{
-		while (len-- > 0)
+		while ((int)--len >= 0)
 			d[len] = s[len];
 	}
 	else
 	{
 		i = -1;
-		while (s[++i] && i < len)
+		while (++i < len)
 			d[i] = s[i];
 	}
 	return (dst);
